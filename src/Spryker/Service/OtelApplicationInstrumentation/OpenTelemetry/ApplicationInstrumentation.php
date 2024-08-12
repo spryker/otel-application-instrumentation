09d8fd6caea1b19053c74547dbe7328f72f9af5b
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-08-12 19:04:59 +0200

    FRW-8618 Fixes

--- src/Spryker/Service/OtelApplicationInstrumentation/OpenTelemetry/ApplicationInstrumentation.php
+++ src/Spryker/Service/OtelApplicationInstrumentation/OpenTelemetry/ApplicationInstrumentation.php
@@ -17,7 +17,6 @@ use OpenTelemetry\Context\Context;
 use OpenTelemetry\Context\ContextStorageScopeInterface;
 use OpenTelemetry\SemConv\TraceAttributes;
 use Spryker\Shared\Application\Application;
-use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
 use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentationInterface;
 use Spryker\Shared\Opentelemetry\Request\RequestProcessorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -66,14 +65,19 @@ class ApplicationInstrumentation implements ApplicationInstrumentationInterface
         CachedInstrumentationInterface $instrumentation,
         RequestProcessorInterface $request
     ): void {
+        // phpcs:disable
         hook(
             class: Application::class,
             function: static::METHOD_NAME,
             pre: static function ($instance, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation, $request): void {
+                if ($instrumentation::getCachedInstrumentation() === null || $request->getRequest() === null) {
+                    return;
+                }
+
                 if (!defined('OTEL_APPLICATION_TRACE_ID')) {
                     define('OTEL_APPLICATION_TRACE_ID', uuid_create());
                 }
-                
+
                 $input = [static::APPLICATION_TRACE_ID_KEY => OTEL_APPLICATION_TRACE_ID];
                 TraceContextPropagator::getInstance()->inject($input);
 
@@ -100,16 +104,18 @@ class ApplicationInstrumentation implements ApplicationInstrumentationInterface
                 static::handleError($scope);
             },
         );
+        // phpcs:enable
     }
 
     /**
      * @param \OpenTelemetry\Context\ContextStorageScopeInterface $scope
      *
-     * @return \OpenTelemetry\API\Trace\Span
+     * @return \OpenTelemetry\API\Trace\SpanInterface
      */
     protected static function handleError(ContextStorageScopeInterface $scope): SpanInterface
     {
         $error = error_get_last();
+        $exception = null;
 
         if (is_array($error) && in_array($error['type'], [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE], true)) {
             $exception = new Exception(
@@ -120,13 +126,13 @@ class ApplicationInstrumentation implements ApplicationInstrumentationInterface
         $scope->detach();
         $span = Span::fromContext($scope->context());
 
-        if (isset($exception)) {
+        if ($exception !== null) {
             $span->recordException($exception);
         }
 
-        $span->setAttribute(static::ERROR_MESSAGE, isset($exception) ? $exception->getMessage() : '');
-        $span->setAttribute(static::ERROR_CODE, isset($exception) ? $exception->getCode() : '');
-        $span->setStatus(isset($exception) ? StatusCode::STATUS_ERROR : StatusCode::STATUS_OK);
+        $span->setAttribute(static::ERROR_MESSAGE, $exception !== null ? $exception->getMessage() : '');
+        $span->setAttribute(static::ERROR_CODE, $exception !== null ? $exception->getCode() : '');
+        $span->setStatus($exception !== null ? StatusCode::STATUS_ERROR : StatusCode::STATUS_OK);
 
         $span->end();
 
